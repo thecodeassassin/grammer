74cbcc63d181369c5c580436d01d8b6b740e770e
Stephen <stephen@tca0.nl>
2018-05-28 09:23:24 +0200

    Added required changes for reference

--- README.md
+++ README.md
@@ -14,3 +14,13 @@ minikube service cujoo-node
 Usage in example
 
 http://<ip address>:<port>/adjectives/buddy+guy/mustang+sally
+
+# Improvements
+
+1. It's not advisesable to commit node_modules in your source code repository
+2. Complexity can be greatly reduced by using async/await (https://javascript.info/async-await)
+3. Keep your docker images small, don't send anything to the docker daemon that you don't have to. Use a .dockerignore file
+4. Practise immutable values. See: https://medium.com/javascript-scene/javascript-es6-var-let-or-const-ba58b8dcde75 and https://wecodetheweb.com/2016/02/12/immutable-javascript-using-es6-and-beyond/
+5. Service was not sending proper JSON.
+6. Missing Kubernetes configuration
+7. /healthz endpoint was not working
--- service.js
+++ service.js
@@ -1,113 +1,60 @@
-var Promise = require('promise')
-
-var WordPOS = require('wordpos'),
-  wordpos = new WordPOS()
-
-var Koa = require('koa')
-var Router = require('koa-router')
-
-var app = new Koa()
-var router = new Router()
-
+const Promise = require('promise')
+const WordPOS = require('wordpos')
+const Koa = require('koa')
+const Router = require('koa-router')
 const got = require('got')
+
+const app = new Koa()
+const router = new Router()
+const wordpos = new WordPOS() // declare on seperate lines and always after imports
 
 app.experimental = true
 
-router.get('/:type/:artist/:title', function (ctx, next) {
-  return new Promise(function (resolve, reject) {
-    var type = ctx.params.type
-    var artist = ctx.params.artist
-    var title = ctx.params.title
-    if (type === 'adjectives' || type === 'verbs') {
-      var lera = getsong(type, artist, title)
-      lera.then(function (val) {
-        ctx.body = val
-        resolve()
-      })
-    } else {
-      ctx.body = 'Invalid parameter'
-      resolve()
-    }
-  })
-})
-
-router.get('/:type', function (ctx, next) {
-  var type = ctx.params.type
-  if (type === 'healtzh') {
-    ctx.body = '200 OK'
-  } else {
-    ctx.body = 'Invalid parameter'
-  }
-})
-
-router.get('/', function (ctx, next) {
-  var type = ctx.params.type
-    ctx.body = 'Service usage http://<adress>/query_type/artist/title/'
-
-})
-
-router.post('/:type/:artist/:title', function (ctx, next) {
-  return new Promise(function (resolve, reject) {
-    var type = ctx.params.type
-    var artist = ctx.params.artist
-    var title = ctx.params.title
-    if (type === 'adjectives' || type === 'verbs') {
-      var lera = getsong(type, artist, title)
-      lera.then(function (val) {
-        ctx.body = val
-        resolve()
-      })
-    } else {
-      ctx.body = 'Invalid parameter'
-      resolve()
-    }
-  })
-})
-
-router.post('/:type', function (ctx, next) {
-  var type = ctx.params.type
-  if (type === 'healtzh') {
-    ctx.body = '200 OK'
-  } else {
-    ctx.body = 'Invalid parameter'
-  }
-})
-
-router.post('/', function (ctx, next) {
-  var type = ctx.params.type
-    ctx.body = 'Service usage http://<address>/query_type/artist/title/'
-
-})
-
-function getsong (type, artist, title) {
-  return new Promise(function (resolve, reject) {
-    (async () => {
-      try {
-        const response = await got('https://api.lyrics.ovh/v1/:' + artist + '/:' + title)
-        var lyric = response.body
-        if (type === 'adjectives') {
-          wordpos.getAdjectives(lyric, function (result) {
-            var Jresult = '{artist :' + artist + ', tittle : ' + title + ', type:' + type + ', Adjectives :{ ' + result + '}}'
-            resolve(JSON.stringify(Jresult))
-            // console.log(result);
-          })
-        } else {
-          wordpos.getVerbs(lyric, function (result) {
-            var Jresult = '{artist :' + artist + ', tittle : ' + title + ', type:' + type + ', Verbs :{ ' + result + '}}'
-            resolve(JSON.stringify(Jresult))
-            // console.log(result);
-          })
+router.get('/:type/:artist/:title', async (ctx, next) => {
+    const type = ctx.params.type
+    const artist = ctx.params.artist
+    const title = ctx.params.title
+    try {
+        if (type === 'adjectives' || type === 'verbs') {
+            ctx.body = await getSong(type, artist, title)
+            ctx.status = 200
+            return next()
         }
-      } catch (error) {
-        var resper = error.response.body
-        reject(JSON.stringify(resper))
-      }
-    })()
-  })
+        ctx.body = 'Invalid parameter'
+        ctx.status = 400
+    } catch (err) {
+        console.error(err)
+        ctx.body = err.message
+        ctx.status = 400
+    }
+})
+
+router.get('/healthz', (ctx, next) => {
+    ctx.body = '200 OK'
+    ctx.status = 200
+})
+
+router.get('/', (ctx, next) => {
+    const type = ctx.params.type
+    ctx.body = 'Service usage http://<adress>/query_type/artist/title/'
+})
+
+async function getSong (type, artist, title) {
+    const response = await got('https://api.lyrics.ovh/v1/:' + artist + '/:' + title)
+    const lyric = response.body
+    const method = (type === 'adjectives' ? 'getAdjectives' : 'getVerbs')
+    const words = await wordpos[method](lyric)
+    return {
+        words,
+        artist,
+        title,
+        type,
+    }
 }
 
 app
   .use(router.routes())
   .use(router.allowedMethods())
 
+console.log(`Listening on port 8080`)
 app.listen(8080)
